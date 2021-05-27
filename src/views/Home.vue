86f7418b7dcc93dc420b4108c9d8cc099d910536
Marco Antônio Queiroz <marco.aq7@gmail.com>
2021-05-26 22:06:04 -0300

    New build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="home">Pagina home</div>
   <h1>Home</h1>
+  Saldo: {{ saldo }} <br />
+  Cofre: {{ cofre }}<br />
+  Pagar: {{ pagar }}<br />
+  Receber: {{ receber }}<br />
 </template>
 
 <script>
-import { inject, reactive } from "vue";
+import { inject, computed, reactive } from "vue";
+
+const moment = require("moment");
 
 export default {
   name: "Home",
@@ -14,9 +20,60 @@ export default {
 
     const values = reactive(store.database.values);
 
+    const saldo = computed(() => {
+      return values
+        .filter((o) => o["Escola"] === store.escola.nome)
+        .reduce((acc, val) => {
+          return acc + val["Valor"];
+        }, 0);
+    });
+
+    const cofre = computed(() => {
+      return values
+        .filter((o) => o["Escola"] === store.escola.nome)
+        .filter((o) => o["Local do movimento"] === "Cofre")
+        .reduce((acc, val) => {
+          return acc + val["Valor"];
+        }, 0);
+    });
+
+    const pagar = computed(() => {
+      return values
+        .filter((o) => o["Escola"] === store.escola.nome)
+        .filter((o) => o["ES"] === "Saída")
+        .filter((o) => o["Pago em"] === "")
+        .filter(
+          (o) =>
+            moment(o["Vencimento"]).format("DD/MM/YYYY") ===
+            moment().format("DD/MM/YYYY")
+        )
+        .reduce((acc, val) => {
+          return acc + val["Valor"];
+        }, 0);
+    });
+
+    const receber = computed(() => {
+      return values
+        .filter((o) => o["Escola"] === store.escola.nome)
+        .filter((o) => o["ES"] === "Entrada")
+        .filter((o) => o["Pago em"] === "")
+        .filter(
+          (o) =>
+            moment(o["Vencimento"]).format("DD/MM/YYYY") ===
+            moment().format("DD/MM/YYYY")
+        )
+        .reduce((acc, val) => {
+          return acc + val["Valor"];
+        }, 0);
+    });
+
     return {
       store,
       values,
+      saldo,
+      cofre,
+      pagar,
+      receber,
     };
   },
 };
